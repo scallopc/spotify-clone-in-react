b4d8ea556e4116957d72bb522b21f5f05a69dc29
Jaque <j.costa@dino.com.br>
2023-02-25 15:59:31 -0300

    login

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,5 +1,6 @@
 import { faHeart, faFireFlameCurved, faPlay, faHouse, faFolder, faArrowRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 import { useEffect, useState } from "react";
+import apiClient from "../../spotify";
 import SidebarButton from "./sidebarButton";
 import { Avatar, SidebarContainer } from "./styles";
 
@@ -7,11 +8,12 @@ export default function Sidebar() {
   const [image, setImage] = useState(
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdLAY3C19kL0nV2bI_plU3_YFCtra0dpsYkg&usqp=CAU"
   );
-    // useEffect(() => {
-    //   apiClient.get("me").then((response) => {
-    //     setImage(response.data.images[0].url);
-    //   });
-    // }, []);
+    useEffect(() => {
+      apiClient.get("me").then((response) => {
+        //console.log(response);
+        setImage(response.data.images[0].url);
+      });
+    }, []);
 
   return (
     <SidebarContainer>
