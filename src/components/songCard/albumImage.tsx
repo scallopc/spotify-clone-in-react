e2aa79e1347992fb8986779999a456c81e2bc364
Jaque <j.costa@dino.com.br>
2023-04-20 17:42:36 -0300

    AlbumImage

--- src/components/songCard/albumImage.tsx
+++ src/components/songCard/albumImage.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { AlbumImageContainer, AlbumImageShadow, Image } from "./styles";
 
 export default function AlbumImage({ url }: any) {
-  return <div>albumImage</div>;
+  return (
+    <AlbumImageContainer>
+      <Image src={url} alt="album art" className="albumImage-art" />
+      <AlbumImageShadow>
+        <Image src={url} alt="shadow" className="albumImage-shadow" />
+      </AlbumImageShadow>
+    </AlbumImageContainer>
+  );
 }
